a6728a252665e09e2316b325d593836fae02067d
Dilum Aluthge <dilum@aluthge.com>
2022-03-15 04:59:06 -0400

    Update dict_utils.jl

--- src/dict_utils.jl
+++ src/dict_utils.jl
@@ -26,7 +26,7 @@ function setindex_same_value!(
 
     old_value = dict[key]
     new_value_converted = convert(V, new_value)::V
-    if old_value !== new_value # we require egality
+    if old_value !== new_value_converted # we require egality
         msg = "Duplicate key found"
         @error msg key old_value new_value new_value_converted
         throw(ErrorException(msg))
