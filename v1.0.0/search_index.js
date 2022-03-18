93c3fcd988f15d186f5a5be4dcbd3f6d13d326bc
Documenter.jl <documenter@juliadocs.github.io>
2022-03-18 09:23:09 +0000

    build based on d80deb3

---
+++ v1.0.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DiagnosisClassification","category":"page"},{"location":"#DiagnosisClassification","page":"Home","title":"DiagnosisClassification","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DiagnosisClassification.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DiagnosisClassification]","category":"page"}]
+}
